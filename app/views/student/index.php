41c14fa8495900a19edeb00a5bca86b47a8a3977
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-04-20 09:38:34 +0200

    Removed unnecesary code
    Forms were deleted since they will be created on another branch

--- app/views/student/index.php
+++ app/views/student/index.php
@@ -1,11 +1,15 @@
   <title><?= $data["title"]?></title>
-<h1>Scan je tag</h1>
+<h1>Studenten beheer pagina</h1><br>
 
+
+<!-- Submit form with data -->
 <form action="post">
-    <label for="studentName">Maak een gebruiker aan</label>
-    <input type="text" autofocus id="studentName" name="studentName"><br><br>
+    <label for="studentFirstName">Update de gegevens van een student!</label><br><br>
 </form>
 
 <?php
+
+    // Clean variables
+
     // var_dump($data);
 ?>
