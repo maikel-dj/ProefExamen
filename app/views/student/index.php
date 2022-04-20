1d0dcb23275d358c44af1e399d900f8a377d51d1
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-04-20 09:30:53 +0200

    Reorganized Index.php
    Removed old leftover code

--- app/views/student/index.php
+++ app/views/student/index.php
@@ -2,12 +2,9 @@
 <h1>Scan je tag</h1>
 
 <form action="post">
-    <label for="scanName">Scan your tag</label>
-    <input type="text" autofocus id="scanName" name="scanName"><br><br>
+    <label for="studentName">Maak een gebruiker aan</label>
+    <input type="text" autofocus id="studentName" name="studentName"><br><br>
 </form>
-
-<h2>id  Land    hoofdstad   continent   aantalinwoners</h2>
-<?= $data["scanner"] ?>
 
 <?php
     // var_dump($data);
