449957b69d894a568d53001c5760195a91ce2477
maikel-dj <64279296+maikel-dj@users.noreply.github.com>
2022-04-20 09:29:05 +0200

    Update index.php
    Changed href tag to student/index

--- app/views/index.php
+++ app/views/index.php
@@ -1,2 +1,3 @@
 <h1><a href="http://proefexamen.nl/countries/index">Landenoverzicht</a></h1>
-<h1><a href="http://proefexamen.nl/student/index">Scanner your tag</a></h1>
+<h1><a href="http://proefexamen.nl/student/index">Beheer studenten</a></h1>
+
