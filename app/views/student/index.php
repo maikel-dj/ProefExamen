5c40d966c7c65682d324514f8f65595d385bd81f
Joranvanerk <joranvanerk1@gmail.com>
2022-04-20 14:21:36 +0200

    Visual update, added cards
    Cards have been added to every element, also changed the spacing.

--- app/views/student/index.php
+++ app/views/student/index.php
@@ -1,36 +1,41 @@
   <title><?= $data["title"]?></title>
-<h1>Studenten beheer pagina</h1><br>
 
 <!-- Show list of users with self made table -->
 
-<h3>Alle studenten!</h3>
-<?= $data["title"] ?>
+<div class="card">
+  <div class="card-body">
+    <h3 class="text-center">Alle studenten!</h3>
+    <?= $data["title"] ?>
+    <br>
+    <?= $data["scanner"] ?>
+  </div>
+</div>
 <br>
-<?= $data["scanner"] ?> 
-
-
 <!-- Submit form with data -->
-
-<form action="" method="POST">
-    <br><h3>Update een student uit het systeem!</h3><br> 
-    <input class="form-control" type="text" name="studentid" placeholder="ID Student">
-    <input class="form-control" type="text" name="firstname" placeholder="Voornaam">
-    <input class="form-control" type="text" name="lastname" placeholder="Achternaam">
-    <input class="form-control" type="text" name="mail" placeholder="Mail">
-    <input class="form-control" type="text" name="phonenumber" placeholder="Telefoon nummer">
-    <input class="form-control" type="text" name="class" placeholder="Klas">
-
-    <button class="btn btn-primary" type="submit" name="submitupdate">Confirm</button>
-
-</form>
-
+<div class="card">
+  <div class="card-body">
+    <form action="" method="POST">
+        <br><h3 class="text-center">Update een student uit het systeem!</h3><br>
+        <input class="form-control mb-3" type="text" name="studentid" placeholder="ID Student">
+        <input class="form-control mb-3" type="text" name="firstname" placeholder="Voornaam">
+        <input class="form-control mb-3" type="text" name="lastname" placeholder="Achternaam">
+        <input class="form-control mb-3" type="text" name="mail" placeholder="Mail">
+        <input class="form-control mb-3" type="text" name="phonenumber" placeholder="Telefoon nummer">
+        <input class="form-control mb-3" type="text" name="class" placeholder="Klas">
+        <button class="btn btn-primary" type="submit" name="submitupdate">Confirm</button>
+    </form>
+  </div>
+</div>
+<br>
 
 
 <!-- Delete student -->
-
-<form action="" method="POST">
-    <br><h3>Verwijder een student uit het systeem!.</h3><br> 
-    <input class="form-control" type="text" name="studentid" placeholder="ID Student">
-    <button class="btn btn-primary" type="submit" name="submitdelete">Confirm</button>
-</form>
-
+<div class="card">
+  <div class="card-body">
+    <form action="" method="POST">
+        <br><h3 class="text-center">Verwijder een student uit het systeem!.</h3><br>
+        <input class="form-control mb-3" type="text" name="studentid" placeholder="ID Student">
+        <button class="btn btn-primary mb-3" type="submit" name="submitdelete">Confirm</button>
+    </form>
+  </div>
+</div>
