9e744efe1094ca58a78df462da4f3f0f73ad8ebe
Joranvanerk <joranvanerk1@gmail.com>
2022-04-20 12:27:44 +0200

    Inserted the landing page, more content is being added.
    Added a fast navigation menu and two extra cards, content can be added later.

--- app/views/index.php
+++ app/views/index.php
@@ -1,3 +1,56 @@
-<h1><a href="http://proefexamen.nl/countries/index">Landenoverzicht</a></h1>
-<h1><a href="http://proefexamen.nl/student/index">Beheer studenten</a></h1>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<style media="screen">
+#snelmenu:hover {
+  transition: 0.2s;
+}
 
+#snelmenubg:hover {
+  background-color: #e0e4ff;
+  transition: 0.2s;
+  padding-bottom: 3px;
+  padding-top: 3px;
+  padding-left: 15px;
+}
+
+#snelmenubg{
+  transition: 0.2s;
+  padding-bottom: 3px;
+  padding-top: 3px;
+}
+
+#snelmenu {
+  transition: 0.2s;
+}
+</style>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12 col-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="text-center">Snelmenu</h5>
+          <div id="snelmenubg"><a href="../student/index" id="snelmenu" style="text-decoration: none;"><i class="fas fa-globe"></i> Beheer studenten</a></div>
+          <div id="snelmenubg"><a href="../" id="snelmenu" style="text-decoration: none;"><i class="fas fa-text-width"></i> Beheer baliemedewerkers</a></div>
+          <div id="snelmenubg"><a href="../" id="snelmenu" style="text-decoration: none;"><i class="fas fa-ticket-alt"></i> Beheer docenten</a></div>
+          <div id="snelmenubg"><a href="../" id="snelmenu" style="text-decoration: none;"><i class="fas fa-file-invoice"></i> Maak item</a></div>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-12 col-md-3">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="text-center">Snelmenu</h5>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-12 col-md-6">
+      <div class="card">
+        <div class="card-body">
+          <h5 class="text-center">Snelmenu</h5>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
